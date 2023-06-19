70448c70ec8a685b1f36cb5344a3739ea252329d
RageNeko26 <rneko2006@gmail.com>
2023-06-19 13:56:41 +0700

    fix: add data

--- dom/script.js
+++ dom/script.js
@@ -1,13 +1,25 @@
-import data from "./data.js"
+import menu from "./data.js"
 
 const container = document.querySelector("#menu")
+const btn = document.querySelector("#add")
+const fieldInput = document.querySelector("#fieldInput")
 
 const render = () => {
+    container.innerHTML = ""
     menu.forEach(data => {
         const list = document.createElement("li")
-        list.innerText = data.name
+        list.innerText = data
     
         container.appendChild(list)
     })
 }
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault()
+    menu.push(fieldInput.value)
+    render()
+    fieldInput.value = ""
+})
+
+render()
 
