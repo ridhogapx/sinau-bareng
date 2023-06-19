3c6fea2c04c5ff1fbe3bfb9a3e17865d9bb728a1
RageNeko26 <rneko2006@gmail.com>
2023-06-19 14:06:57 +0700

    feat: adding button for delete data

--- dom/script.js
+++ dom/script.js
@@ -6,10 +6,21 @@ const fieldInput = document.querySelector("#fieldInput")
 
 const render = () => {
     container.innerHTML = ""
-    menu.forEach(data => {
+    menu.forEach((data, i) => {
         const list = document.createElement("li")
+        const deleteBtn = document.createElement("button")
+
+        deleteBtn.innerText = "Hapus"
+        
         list.innerText = data
-    
+
+        deleteBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+            menu.splice(i)
+            render()
+        })
+
+        list.appendChild(deleteBtn)    
         container.appendChild(list)
     })
 }
