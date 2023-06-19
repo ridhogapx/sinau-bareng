c517d3f79f6d98a430f3d8073e1697fb2b81333f
RageNeko26 <rneko2006@gmail.com>
2023-06-19 12:39:02 +0700

    feat: Adding data menu

---
+++ dom/script.js
@@ -0,0 +1,19 @@
+const menu = [
+    {
+        id: 1,
+        name: "Bakso"
+    },
+    {
+        id: 2,
+        name: "Mie Ayam"
+    },
+    {
+        id: 3,
+        name: "Es Teh"
+    },
+    {
+        id: 4,
+        name: "Es Jeruk"
+    }
+]
+
